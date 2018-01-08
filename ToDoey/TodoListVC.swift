170790013c7ba765da6a1c5bf315307b8c1b4298
Oguz <oguzkaratoruk@yandex.com>
2018-01-08 23:32:18 +0300

    todolist complete

---
+++ ToDoey/TodoListVC.swift
@@ -0,0 +1,42 @@
+//
+//  ViewController.swift
+//  ToDoey
+//
+//  Created by Oguz on 5.01.2018.
+//  Copyright © 2018 Oguz Karatoruk. All rights reserved.
+//
+
+import UIKit
+
+class TodoListVC: UITableViewController {
+    let itemArray = ["ispanyolca kelime ogren","yazilim calis","kitap oku"]
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    //MARK - Tableview Datasource methods
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "todoitemcell", for: indexPath)
+        cell.textLabel?.text = itemArray[indexPath.row]
+        return cell
+    }
+    
+    //MARK - Tableview Delegate Methods
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print(itemArray[indexPath.row])
+        
+        if  tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        }else {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+}
+
